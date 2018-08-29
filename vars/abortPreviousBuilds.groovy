759ca6a753c2348ae770b602445c9824043c7e8a
Leonard Lee <sheeeng@gmail.com>
2018-08-29 13:35:57 +0200

    Update abortPreviousBuilds.groovy

--- vars/abortPreviousBuilds.groovy
+++ vars/abortPreviousBuilds.groovy
@@ -5,12 +5,10 @@ import jenkins.model.CauseOfInterruption.UserInterruption
 def call() {
     shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
     echo shortCommit
+    
     Run previousBuild = currentBuild.rawBuild.getPreviousBuildInProgress()
-
     while (previousBuild != null) {
         if (previousBuild.isInProgress()) {
-            previousShortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
-            echo previousShortCommit
             def executor = previousBuild.getExecutor()
             if (executor != null) {
                 echo ">> Aborting older build #${previousBuild.number}."
