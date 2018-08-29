c1c179dc5bd3a7f967e6d96d028c4a955d94fb8d
Leonard Lee <sheeeng@gmail.com>
2018-08-29 15:14:13 +0200

    Create getTriggerCause.groovy

---
+++ vars/getTriggerCause.groovy
@@ -0,0 +1,19 @@
+import hudson.model.Result
+import hudson.model.Run
+import jenkins.model.CauseOfInterruption.UserInterruption
+
+def call() {
+    // https://stackoverflow.com/a/49901413/4763512
+    Run previousBuild = currentBuild.rawBuild.getPreviousBuildInProgress()
+        currentBuild.rawBuild.getCauses().each {
+        if (cause instanceof Cause.UserIdCause) {
+            echo ">> Cause.UserIdCause"
+        }
+        if (cause instanceof Cause.RemoteCause) {
+            echo ">> Cause.RemoteCause"
+        }
+        if (cause instanceof Cause.UpstreamCause) {
+            echo ">> Cause.UpstreamCause"
+        }
+    }
+}
