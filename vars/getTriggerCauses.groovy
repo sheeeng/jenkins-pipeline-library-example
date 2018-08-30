1c4a236272cdcc64538e9b2415cd502086f24f9e
Leonard Lee <leonard.lee@praqma.com>
2018-08-30 08:10:09 +0200

    Update file.

--- vars/getTriggerCauses.groovy
+++ vars/getTriggerCauses.groovy
@@ -5,7 +5,8 @@ import jenkins.model.CauseOfInterruption.UserInterruption
 def call() {
     // https://stackoverflow.com/a/49901413/4763512
     Run previousBuild = currentBuild.rawBuild.getPreviousBuildInProgress()
-        currentBuild.rawBuild.getCauses().each {
+    currentBuild.rawBuild.getCauses().each {
+        println ">> Cause:" cause
         if (cause instanceof Cause.UserIdCause) {
             echo ">> Cause.UserIdCause"
         }
