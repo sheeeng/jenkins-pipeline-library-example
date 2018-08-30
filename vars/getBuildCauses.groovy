94219d0295826011bdbc4014d65c01165129c412
Leonard Lee <leonard.lee@praqma.com>
2018-08-30 09:10:21 +0200

    Update file.

--- vars/getBuildCauses.groovy
+++ vars/getBuildCauses.groovy
@@ -14,13 +14,28 @@ def call() {
     for (Cause cause : causes) {
         if (cause instanceof Cause.UserIdCause) {
             println ">> Cause.UserIdCause"
+            // https://javadoc.jenkins-ci.org/hudson/model/Cause.UserIdCause.html
             println cause.getShortDescription()
+            println cause.getUserId()
+            println cause.getUserName()
+            println cause.getUserUrl()
         }
         else if (cause instanceof Cause.RemoteCause) {
             println ">> Cause.RemoteCause"
+            // https://javadoc.jenkins-ci.org/hudson/model/Cause.RemoteCause.html
+            println cause.getAddr()
+            println cause.getNote()
+            println cause.getShortDescription()
         }
         else if (cause instanceof Cause.UpstreamCause) {
             println ">> Cause.UpstreamCause"
+            // https://javadoc.jenkins-ci.org/hudson/model/Cause.UpstreamCause.html
+            println	getShortDescription()
+            println getUpstreamBuild()
+            println getUpstreamCauses().size()
+            println getUpstreamProject()
+            println getUpstreamRun().getUrl()
+            println getUpstreamUrl()
         }
         else {
             println ">> Unknown causes!"
